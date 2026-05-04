c9ed9989af9d4289f57e37a24819d0dea3259b13
AndersonPulidoA <103030289+AndersonPulidoA@users.noreply.github.com>
2026-05-04 15:24:31 -0500

    Update agregaciones.js

--- consultas/agregaciones.js
+++ consultas/agregaciones.js
@@ -1,1 +1,47 @@
+*/Contar documentos:
+  
+[
+ {
+  $count: "total_publicaciones"
+ }
+]
 
+*/Resultado esperado:
+
+{ "total_publicaciones": 100 }
+
+
+*/Promedio de likes:
+  
+[
+ {
+  $group: {
+    _id: null,
+    promedio_likes: { $avg: "$likes" }
+  }
+ }
+]
+
+*/Suma de comentarios:
+  
+[
+ {
+  $group: {
+    _id: null,
+    total_comentarios: { $sum: "$comentarios" }
+  }
+ }
+]
+
+
+*/Likes por usuario (análisis más completo):
+
+  
+[
+ {
+  $group: {
+    _id: "$usuario_id",
+    total_likes: { $sum: "$likes" }
+  }
+ }
+]
