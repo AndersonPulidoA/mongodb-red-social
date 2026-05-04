7d8a8d4399a3b45834861ce19d5fb2c0cf1c3403
AndersonPulidoA <103030289+AndersonPulidoA@users.noreply.github.com>
2026-05-04 15:15:10 -0500

    Update consultas_basicas.js

--- consultas/consultas_basicas.js
+++ consultas/consultas_basicas.js
@@ -1,1 +1,14 @@
 
+#Consultasbásicas
+
+#Selección
+db.publicaciones.find()
+
+#Actualización
+db.publicaciones.updateOne(
+ {usuario_id: 1},
+ {$set: {likes: 20}}
+)
+
+#Eliminación
+db.publicaciones.deleteOne({usuario_id: 2})
